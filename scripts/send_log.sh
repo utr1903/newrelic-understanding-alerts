69fcb8f82751ce2b0f9dad77ef4a00eac8091915
uturkarslan <ugurturkarslan@gmail.com>
2023-02-11 20:05:12 +0100

    Add case number as arg

--- scripts/send_log.sh
+++ scripts/send_log.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
 
+# Get commandline arguments
+while (( "$#" )); do
+  case "$1" in
+    --case)
+      case="$2"
+      shift
+      ;;
+    *)
+      shift
+      ;;
+  esac
+done
+
+if [[ $case == "" ]]; then
+  echo "Case number not given!"
+  exit 1
+fi
+
 curl -X POST https://log-api.eu.newrelic.com/log/v1 \
   -H "Content-Type: application/json" \
   -H "Api-Key: ${NEWRELIC_LICENSE_KEY}" \
   -d '[{
     "logs": [{
-        "message": "Understanding New Relic alerting - case01"
+        "message": "Understanding New Relic alerting - case'${case}'"
       }]
     }]'
